904ceaf252de5cec4bcae5a19a1d825108bc1bdd
Dushan Eranda <dushaneranda@083gmail.com>
2026-09-06 20:50:12 +0530

    upload header

---
+++ backend/head-lib.php
@@ -0,0 +1,17 @@
+<?
+if($apploaded){
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<!-- font awesome -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!--Bootstrap css and js-->
+<title>Student Resource Hub</title>
+<link rel="icon" type="image/png" href="site-img/fav.png">
+
+<link rel="stylesheet" type="text/css" href="css/root.css">
+<link rel="stylesheet" type="text/css" href="css/catdesign.css">
+<?}else{
+	header("Location: index.php");
+}?>
